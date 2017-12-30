800c99a39010696e70db90873642cee64be3cd5d
mryyomutga <mryyomutga@gmail.com>
2017-12-31 00:30:48 +0900

    ListViewでタイムラインを表示

--- README.md
+++ README.md
@@ -4,26 +4,30 @@
 
 ## 機能
 
-- アプリ起動時にOAuth認証をして自分のTwitterアカウントと連携する
-- Tweet機能
-- 自分のタイムラインを3件取得して表示
+- ✔アプリ起動時にOAuth認証をして自分のTwitterアカウントと連携する
+- ✔Tweet機能
+- ✔自分のタイムラインを3件取得して表示
 - 写真付きTweet
 
 ## 画面構成
 
-![view.img](https://github.com/mryyomutga/https://github.com/mryyomutga/OnewayTwitter/image/view.png)
+![view.img](https://github.com/mryyomutga/OnewayTwitter/blob/master/image/view.png)
 
 ## Libraries
 
-- twitter4j-core-4.0.4
+- [twitter4j-core-4.0.4](http://twitter4j.org/ja/index.html)
+- [android-smart-image-view-1.0.0](http://loopj.com/android-smart-image-view/)
 
 ## 参考
 
 - http://twitter4j.org/ja/index.html
 - https://akira-watson.com/android/activity-1.html
 - https://qiita.com/HideMatsu/items/2e6caec8265bcf2a2dcb
+- https://qiita.com/icchi_h/items/8ce738ce8511ef69c799
+- http://ojed.hatenablog.com/entry/2015/12/05/161013
 - https://qiita.com/Yuki_Yamada/items/f8ea90a7538234add288
 - https://qiita.com/kskso9/items/1ee5b1dbe996402ae94b
+- https://qiita.com/Tsumugi/items/47f31bb7351979a45653
 - http://mltmdkana.hatenablog.com/entry/2016/11/04/234003
 - http://mltmdkana.hatenablog.com/entry/2016/11/06/143739
 - https://qiita.com/gabu/items/673288c3a5b39f89aa92
--- app/src/main/java/jp/ac/kanazawait/onewaytwitter/MainActivity.java
+++ app/src/main/java/jp/ac/kanazawait/onewaytwitter/MainActivity.java
@@ -1,16 +1,23 @@
 package jp.ac.kanazawait.onewaytwitter;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.*;
 import android.view.View.*;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.loopj.android.image.SmartImageView;
 
 import java.util.Collections;
 import java.util.List;
@@ -23,6 +30,8 @@ import twitter4j.TwitterFactory;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     List<Status> statuses = Collections.emptyList();
+    private TweetItem tweetItem;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,35 +46,66 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         }
 
         Twitter twitter = TwitterUtils.getTwitterInstance(this);
+        tweetItem = new TweetItem(this);
+        listView = (ListView) findViewById(R.id.tweet_list);
 
-        getTimeLine(twitter);
+        tweetItem.getTimeLine(twitter);
 
         findViewById(R.id.transition_tweet_activity).setOnClickListener(this);
     }
 
-    @SuppressLint("StaticFieldLeak")
-    private void getTimeLine(final Twitter twitter) {
-        new AsyncTask<Void, Void, List<Status>>() {
-            @Override
-            protected List<twitter4j.Status> doInBackground(Void... voids) {
-                // Pagingの設定(要はリクエスト時に含めるパラメータ)
-                Paging paging = new Paging();
-                paging.setCount(3);             // つぶやきを3件取得
-                try {
-                    statuses = twitter.getUserTimeline(paging);
-                    return statuses;
-                } catch (TwitterException e) {
-                    e.printStackTrace();
-                }
-                return null;
+    private class TweetItem extends ArrayAdapter<Status> {
+        private LayoutInflater inflater;
+
+        public TweetItem(@NonNull Context context) {
+            super(context, R.layout.tweet_item_view);
+            inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.tweet_item_view, null);
             }
-            // 取得したタイムラインのふるまい
-            protected void onPostExecute(List<twitter4j.Status> statuses) {
-                for(twitter4j.Status status : statuses) {
-                    showToast(status.getText());
+            Status item = getItem(position);
+            TextView name = (TextView) convertView.findViewById(R.id.name);
+            name.setText(item.getUser().getName());
+            TextView screenName = (TextView) convertView.findViewById(R.id.screen_name);
+            screenName.setText("@" + item.getUser().getScreenName());
+            TextView text = (TextView) convertView.findViewById(R.id.text);
+            text.setText(item.getText());
+            SmartImageView icon = (SmartImageView) convertView.findViewById(R.id.icon);
+            icon.setImageUrl(item.getUser().getProfileImageURL());
+            return convertView;
+        }
+
+        @SuppressLint("StaticFieldLeak")
+        private void getTimeLine(final Twitter twitter) {
+            new AsyncTask<Void, Void, List<twitter4j.Status>>() {
+                @Override
+                protected List<twitter4j.Status> doInBackground(Void... params) {
+                    // Pagingの設定(要はリクエスト時に含めるパラメータ)
+                    Paging paging = new Paging();
+                    paging.setCount(3);             // つぶやきを3件取得
+                    try {
+                        return twitter.getUserTimeline(paging);
+                    } catch (TwitterException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
                 }
-            }
-        }.execute();
+                // 取得したタイムラインのふるまい
+                protected void onPostExecute(List<twitter4j.Status> statuses) {
+                    if (statuses != null) {
+                        for (twitter4j.Status status : statuses) {
+                            tweetItem.add(status);
+                            showToast(status.getText());
+                        }
+                        listView.setAdapter(tweetItem);
+                    }
+                }
+            }.execute();
+        }
     }
 
     @Override
