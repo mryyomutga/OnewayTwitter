34efa707004a9146cea397cd850759bdb97573e4
mryyomutga <mryyomutga@gmail.com>
2017-12-30 19:29:56 +0900

    ツイート機能の実装

--- README.md
+++ README.md
@@ -12,3 +12,17 @@
 ## Libraries
 
 - twitter4j-core-4.0.4
+
+## 参考
+
+- http://twitter4j.org/ja/index.html
+- https://akira-watson.com/android/activity-1.html
+- https://qiita.com/HideMatsu/items/2e6caec8265bcf2a2dcb
+- https://qiita.com/Yuki_Yamada/items/f8ea90a7538234add288
+- https://qiita.com/kskso9/items/1ee5b1dbe996402ae94b
+- http://mltmdkana.hatenablog.com/entry/2016/11/04/234003
+- http://mltmdkana.hatenablog.com/entry/2016/11/06/143739
+- https://qiita.com/gabu/items/673288c3a5b39f89aa92
+- https://qiita.com/gabu/items/53857fcfa871b921af45
+- https://qiita.com/kskso9/items/1ee5b1dbe996402ae94b
+- https://akira-watson.com/android/asynctask.html
--- app/src/main/java/jp/ac/kanazawait/onewaytwitter/TweetActivity.java
+++ app/src/main/java/jp/ac/kanazawait/onewaytwitter/TweetActivity.java
@@ -1,12 +1,18 @@
 package jp.ac.kanazawait.onewaytwitter;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.*;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 
 /**
  * Created by mryyomutga on 2017/12/29.
@@ -14,13 +20,47 @@ import twitter4j.Twitter;
 
 public class TweetActivity extends Activity implements OnClickListener {
     private Twitter twitter;
+    private EditText tweets;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet);
         twitter = TwitterUtils.getTwitterInstance(this);
 
+        tweets = (EditText) findViewById(R.id.tweet_text);
+
         findViewById(R.id.tweet_button).setOnClickListener(this);
+    }
+
+    private void tweet(){
+        new AsyncTask<String, Void, Boolean>() {
+            @Override
+            protected Boolean doInBackground(String... params) {
+                try {
+                    twitter.updateStatus(params[0]);
+                    return true;
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Boolean result){
+                if (result) {
+                    showToast("ツイートが完了しました!! (・ω・)ノ");
+                } else {
+                    showToast("ツイートが失敗しました… (T_T)");
+                }
+            }
+        }.execute(tweets.getText().toString());
+
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -29,6 +69,7 @@ public class TweetActivity extends Activity implements OnClickListener {
             switch (v.getId()) {
                 // Click Tweet Button
                 case R.id.tweet_button:
+                    tweet();
                     Intent mainIntent = new Intent(getApplication(), MainActivity.class);
                     startActivity(mainIntent);
                     finish();
