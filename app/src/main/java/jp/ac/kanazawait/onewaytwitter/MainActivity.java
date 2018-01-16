e13b784780518e76a8a92ec3e6837b17f49fa8d7
mryyomutga <mryyomutga@gmail.com>
2018-01-17 03:40:27 +0900

    アプリの外観&細かいバグの修正

--- app/src/main/java/jp/ac/kanazawait/onewaytwitter/MainActivity.java
+++ app/src/main/java/jp/ac/kanazawait/onewaytwitter/MainActivity.java
@@ -2,7 +2,6 @@ package jp.ac.kanazawait.onewaytwitter;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
@@ -12,26 +11,25 @@ import android.content.Intent;
 import android.view.*;
 import android.view.View.*;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.image.SmartImageView;
 
-import java.util.Collections;
 import java.util.List;
 
 import twitter4j.Paging;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import twitter4j.User;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
-    List<Status> statuses = Collections.emptyList();
+    private Twitter twitter;
     private TweetItem tweetItem;
     ListView listView;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,16 +42,55 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             startActivity(intent);
             finish();
         } else {
-            Twitter twitter = TwitterUtils.getTwitterInstance(this);
+            twitter = TwitterUtils.getTwitterInstance(this);
             tweetItem = new TweetItem(this);
             listView = (ListView) findViewById(R.id.tweet_list);
 
             tweetItem.getTimeLine(twitter);
 
+            getAccountInfo();
+
             findViewById(R.id.transition_tweet_activity).setOnClickListener(this);
         }
+    }
 
-
+    // アカウント情報の設定
+    private void getAccountInfo() {
+        new AsyncTask<Void, Void, User>() {
+            @Override
+            protected User doInBackground(Void... voids) {
+                user = null;
+                try {
+                    user = twitter.verifyCredentials();
+                    return user;
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+            protected void onPostExecute(User user) {
+                if (user != null) {
+                    // name
+                    TextView name = (TextView) findViewById(R.id.name);
+                    name.setText(user.getName());
+                    // screen Name
+                    TextView screenName = (TextView) findViewById(R.id.screen_name);
+                    screenName.setText("@" + user.getScreenName());
+                    // description
+                    TextView description = (TextView) findViewById(R.id.description);
+                    description.setText(user.getDescription());
+                    // friends
+                    TextView friends = (TextView) findViewById(R.id.friends);
+                    friends.setText("Friends : " + user.getFriendsCount());
+                    // followers
+                    TextView followers = (TextView) findViewById(R.id.followers);
+                    followers.setText("Followers : " + user.getFollowersCount());
+                    // icon
+                    SmartImageView icon = (SmartImageView) findViewById(R.id.icon);
+                    icon.setImageUrl(user.getProfileImageURL());
+                }
+            }
+        }.execute();
     }
 
     private class TweetItem extends ArrayAdapter<Status> {
@@ -70,17 +107,22 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 convertView = inflater.inflate(R.layout.tweet_item_view, null);
             }
             Status item = getItem(position);
+            // name
             TextView name = (TextView) convertView.findViewById(R.id.name);
             name.setText(item.getUser().getName());
+            // screen name
             TextView screenName = (TextView) convertView.findViewById(R.id.screen_name);
             screenName.setText("@" + item.getUser().getScreenName());
+            // tweet
             TextView text = (TextView) convertView.findViewById(R.id.text);
             text.setText(item.getText());
+            // icon
             SmartImageView icon = (SmartImageView) convertView.findViewById(R.id.icon);
             icon.setImageUrl(item.getUser().getProfileImageURL());
             return convertView;
         }
 
+        // タイムラインの取得
         @SuppressLint("StaticFieldLeak")
         private void getTimeLine(final Twitter twitter) {
             new AsyncTask<Void, Void, List<twitter4j.Status>>() {
